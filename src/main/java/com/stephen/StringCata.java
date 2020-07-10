2e9b4bea6ef3549a027b7f99bcea837ad43be830
Dave Sugden <dave.sugden@shopify.com>
2020-07-10 13:20:27 -0400

    first exercise

--- src/main/java/com/stephen/StringCata.java
+++ src/main/java/com/stephen/StringCata.java
@@ -15,4 +15,20 @@ public class StringCata {
     public String input() {
         return this.input;
     }
+
+    /**
+     * First exercise:  add a method called parseDigits that parses out any digit from the input String.
+     * eg.
+     *  input = "adh4mn"
+     *  parseDigits()  returns "4" as a String
+     *
+     * input = "adh4m333ter5n"
+     * parseDigits()  returns "43335" as a String
+     *
+     * Google how to break a String into a Char[] (array of characters), then loop
+     * over this array and pick out the numbers, and return them as a new String.
+     *
+     */
+
+
 }
