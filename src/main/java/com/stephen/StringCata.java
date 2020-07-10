5cf500f88d914b5719ae0d6f6ca7144f8c798e0a
Dave Sugden <dave.sugden@shopify.com>
2020-07-10 13:00:48 -0400

    StringCata

---
+++ src/main/java/com/stephen/StringCata.java
@@ -0,0 +1,14 @@
+package com.stephen;
+
+public class StringCata {
+
+    private String input = null;
+
+    public StringCata(String input){
+        this.input = input;
+    }
+
+    public String input() {
+        return this.input;
+    }
+}
---
+++ src/test/java/com/stephen/StringCataTest.java
@@ -0,0 +1,18 @@
+package com.stephen;
+
+import org.junit.Test;
+
+public class StringCataTest {
+
+    @Test
+    public void testParseOne() {
+
+        StringCata stringCata = new StringCata("aqwhj332mb");
+
+        assert(stringCata.input() == "aqwhj332mb");
+
+
+    }
+
+
+}
